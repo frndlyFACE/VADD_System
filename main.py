5221ba89a046a89599f7b4ac553527a0a17ae36c
nofx <kartikeyasrivastava2002@gmail.com>
2023-10-19 08:34:43 +0530

    bug fix

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def load_user(user_id):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
-    email = db.Column(db.String(120), nullable=False, unique=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
     role = db.Column(db.String(20), default='user', nullable=False)
